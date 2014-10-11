531a8093f04d5dc9f6a9504602e3452da545ae09
oronain <oronain1623@gmail.com>
2014-10-11 22:41:06 +0900

    すべてプッシュ

---
+++ Assets/script/camera.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+    /*if (Input.GetKey(KeyCode.J)){
+        transform.Translate(transform.right * -0.1);
+    }
+    if (Input.GetKey(KeyCode.L)){
+        transform.Translate(transform.right * 0.1);
+    }*/
+    if(Input.GetAxis ("Horizontal")) transform.Rotate(0, 100 * Time.deltaTime * Input.GetAxis ("Horizontal"), 0);
+    /*if (Input.GetKey(KeyCode.I)){
+        transform.Translate(transform.forward * 0.1);
+    }
+    if (Input.GetKey(KeyCode.K)){
+        transform.Translate(transform.forward *- 0.1);
+    }*/
+}
